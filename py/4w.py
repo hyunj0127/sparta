498bdf447c9b76d093504cc8c97e3a0426f0b628
hyunj0127 <61870283+hyunj0127@users.noreply.github.com>
2020-04-02 17:45:43 +0900

    4주차_과정
    중간점검용

---
+++ py/4w.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
+
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+## API 역할을 하는 부분
+@app.route('/orders', methods=['POST'])
+def write_orders():
+    name_receive = request.form['name_give']
+    quantity_receive = request.form['quantity_give']
+    address_receive = request.form['address_give']
+    mobile_receive = request.form['mobile_give']
+
+    order = {
+        'name': name_receive,
+        'quantity': quantity_receive,
+        'address': address_receive,
+        'mobile': mobile_receive
+    }
+
+    db.orders.insert_one(order)
+    return jsonify({'result':'success','msg':'주문사항이 성공적으로 작성되었습니다.'})
+
+@app.route('/orders', methods=['GET'])
+def read_orders():
+    orders = list(db.orders.find({},{'_id':0}))
+    return jsonify({'result':'success', 'orders': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True)
